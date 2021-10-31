90f7b84cf71e0ba45cc381b7533d8422534e6e3b
Josh Soref <jsoref@users.noreply.github.com>
2021-10-30 21:04:38 -0400

    spelling: occurrences

--- include/asterisk/strings.h
+++ include/asterisk/strings.h
@@ -1430,7 +1430,7 @@ int ast_strings_match(const char *left, const char *op, const char *right);
  * \warning The original string and *buffer will be modified.
  *
  * \details
- * Both '\n' and '\r\n' are treated as single delimiters but consecutive occurrances of
+ * Both '\n' and '\r\n' are treated as single delimiters but consecutive occurrences of
  * the delimiters are NOT considered to be a single delimiter.  This preserves blank
  * lines in the input.
  *
